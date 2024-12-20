8d4282440ff74cc4e7801a3369267958d5612264
GG_Host <gd.host@yandex.ru>
2024-12-20 18:09:07 +0300

    feat: initial commit

---
+++ src/main/java/org/example/sokcsaccounting/SokcsAccountingApplication.java
@@ -0,0 +1,13 @@
+package org.example.sokcsaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokcsAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SokcsAccountingApplication.class, args);
+    }
+
+}
